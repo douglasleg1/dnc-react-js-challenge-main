14636a56e22785bfc1bff2035f3034345336c6b5
Douglas Dantas <douglasdantas@MacBook-Pro-de-Douglas.local>
2023-07-05 12:44:40 -0300

    feat: adding alert response for all deletions

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ function App() {
 
   function deleteAllTasks(){
     setTodoList(todoList.filter((todo) => todo.id === null));
+    alert("Todas as tasks foram deletadas!");
   }
 
   function openModal(typeOpenModal, taskId){
